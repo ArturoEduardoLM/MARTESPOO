3405b8726cce73403acffa32275a3d0942c2efe6
ArturoEduardoLM <113371865+ArturoEduardoLM@users.noreply.github.com>
2023-06-13 15:10:41 -0600

    Add files via upload

---
+++ cosas.py
@@ -0,0 +1,6 @@
+class Alumno:
+    facultad= "FES Aragon"
+    def __init__(self, nombre, carrera, edad):
+        self.nombre =nombre
+        self.carrera= carrera
+        self.edad= edad
---
+++ main.py
@@ -0,0 +1,7 @@
+from aritmetica import suma, resta
+def main():
+    resultado = suma(3,3)
+    print(resultado)
+    print (resta(5,3))
+
+main()
